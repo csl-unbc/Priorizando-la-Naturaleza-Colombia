af3b2245abb4a7948f994277354588e50bc056b3
Xavier C. Llano <xavier.corredor.llano@gmail.com>
2025-03-12 19:13:03 -0500

    Added some scripts to prepare the data, binary and float, tested with Nacional config

---
+++ inst/scripts/utils/prepare_binary_layer.R
@@ -0,0 +1,41 @@
+# Pre-processing -----------------------------------------------------------
+# This script is used to prepare binary layers
+# Binary data:
+#   1: data
+#   0: nodata
+#   255: NA
+
+# Load the terra package
+library(terra)
+
+# Get the raster file path and output path from the command-line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+# Check if the arguments are provided
+if (length(args) < 2) {
+  stop("Please provide the path to the raster file and the output path as arguments.")
+}
+
+# Path to the input raster file
+raster_path <- args[1]
+raster_data <- terra::rast(raster_path)
+
+# Path to the output raster file
+out_path <- args[2]
+
+PU <- terra::rast(file.path("PU_Nacional_1km.tif"))
+mask_data <- terra::clamp(PU, 0, 1)
+
+# apply the extent of the mask to the raster data using first extend and crop
+raster_data <- terra::extend(raster_data, mask_data)
+raster_data <- terra::crop(raster_data, ext(mask_data))
+
+# apply the mask to the raster data
+raster_data <- terra::clamp(raster_data, 0, 255, datatype = "INT1U")
+raster_data[is.na(raster_data)] <- 0
+raster_data[is.na(mask_data)] <- 255
+raster_data[raster_data == 255] <- NA
+
+writeRaster(raster_data, out_path, datatype = "INT1U", overwrite = TRUE)
+
+print(paste("Finish:", out_path, "has been saved."))
---
+++ inst/scripts/utils/prepare_float_layer.R
@@ -0,0 +1,34 @@
+# Pre-processing -----------------------------------------------------------
+# This script is used to prepare float layers
+
+# Load the terra package
+library(terra)
+
+# Get the raster file path and output path from the command-line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+# Check if the arguments are provided
+if (length(args) < 2) {
+  stop("Please provide the path to the raster file and the output path as arguments.")
+}
+
+# Path to the input raster file
+raster_path <- args[1]
+raster_data <- terra::rast(raster_path)
+
+# Path to the output raster file
+out_path <- args[2]
+
+PU <- terra::rast(file.path("PU_Nacional_1km.tif"))
+mask_data <- terra::clamp(PU, 0, 1)
+
+# apply the extent of the mask to the raster data using first extend and crop
+raster_data <- terra::extend(raster_data, mask_data)
+raster_data <- terra::crop(raster_data, ext(mask_data))
+
+# apply the mask to the raster data
+raster_data[is.na(mask_data)] <- NA
+
+writeRaster(raster_data, out_path, datatype = "FLT4S", overwrite = TRUE)
+
+print(paste("Finish:", out_path, "has been saved."))
